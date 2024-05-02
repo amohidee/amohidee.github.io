9b4757c9030276ca93f4a748b1ce02b39dffd2b5
Kushaan Misra <kushaanm@linux-01.andrew.cmu.edu>
2024-05-01 21:01:44 -0400

    working, dont know what to do with the radii yet

--- display_grads.py
+++ display_grads.py
@@ -2,9 +2,18 @@ from PIL import Image
 import matplotlib.pyplot as plt 
 import numpy as np
 
+# x  = 768
+# y = 1024
+fname = 'coins'
+
+with open(f"images/{fname}.txt",'r') as f:
+    ln = (f.readline()[:-1]).split(" ")
+    x = int(ln[0])
+    y = int(ln[1])
+    print(y, x)
+
 def proc_file(img_path):
-    x = 768
-    y = 1024
+    print(img_path)
     img = np.zeros((y,x))
     with open(img_path, 'r') as f:
         for i in range(y):
@@ -12,21 +21,67 @@ def proc_file(img_path):
                 inp = int(float(f.readline()[:-1]))
                 # inp = max(inp, 0)
                 # inp = 500 if (inp > 300) else 0
-                img[i][j] = inp
+                if("radsym" in img_path):
+                    # inp = 0 if (inp < 1000) else inp
+                    inp = inp
+                img[i][j] = inp + 1
     plt.imshow(img)
     plt.show()
+
+def proc_side():
+    fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+    xg = np.zeros((y,x))
+    yg = np.zeros((y,x))
+    dir = np.zeros((y,x))
+    nms = np.zeros((y,x))
+    img_path = f"images/{fname}_nmsgrads.txt"
+    with open(img_path, 'r') as f:
+        for i in range(y):
+            for j in range(x):
+                inp = int(float(f.readline()[:-1]))
+                nms[i][j] = inp
+
+    img_path = f"images/{fname}_xgr.txt"
+    with open(img_path, 'r') as f:
+        for i in range(y):
+            for j in range(x):
+                inp = int(float(f.readline()[:-1]))
+                xg[i][j] = inp if(nms[i][j] != 0) else 0
+    img_path = f"images/{fname}_ygr.txt"
+    with open(img_path, 'r') as f:
+        for i in range(y):
+            for j in range(x):
+                inp = int(float(f.readline()[:-1]))
+                yg[i][j] = inp if(nms[i][j] != 0) else 0
+    for i in range(y):
+        for j in range(x):
+            dir[i][j] = np.arctan2(yg[i][j],xg[i][j]) * 180 /np.pi
+    ax1.imshow(yg)
+    ax2.imshow(xg)
+    ax3.imshow(dir)
+    plt.show()
+
         
 
 if __name__ == '__main__':
-    fname = 'coins'
+    print("gray")
     imp = f"images/{fname}_gray.txt"
     proc_file(imp)
+    print("grads")
     imp = f"images/{fname}_grads.txt"
     proc_file(imp)
+    print("nms")
     imp = f"images/{fname}_nmsgrads.txt"
     proc_file(imp)
+    print("radsym")
     imp = f"images/{fname}_radsym.txt"
     proc_file(imp)
+    print("gauss")
     imp = f"images/{fname}_gauss.txt"
     proc_file(imp)
+    print("gaussnms")
+    imp = f"images/{fname}_gaussnms.txt"
+    proc_file(imp)
+    print("dir grads")
+    proc_side()
     
--- sequential.cpp
+++ sequential.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 
 #define PI 3.14159265358979323846
-#define MAX_RADII 20
+#define MAX_RADII 50
 #define Kn 9.9
+#define THRESH 1000
 
 typedef pair<int,int> pii;
 
@@ -59,8 +60,10 @@ void gradient(double** grayscaleImg, double** gradients, double** gradientDir, I
                     yg += grayscaleImg[dy+i-1][dx+j-1] * ySobel[dy][dx];
                 }
             }
-            gradients[i][j] = sqrt(xg*xg + yg*yg);
+            double mag = sqrt(xg*xg + yg*yg);
+            gradients[i][j] = (mag > 200) ? mag : 0;
             gradY[i][j] = yg; gradX[i][j] = xg;
+// ")
             // if(gradients[i][j]){
             //     gradientDir[i][j] = atan(yg/xg);
             //     if(isnan(gradientDir[i][j])) gradientDir[i][j] = PI / 2 * ((yg > 0) - (yg < 0));
@@ -109,9 +112,14 @@ void radialSymmetry(double **gradX, double** gradY, double **gradients, Image *c
                    double ***O, double ***M){
     for(int i = 0; i < color->h; i++){
         for(int j = 0; j < color->w ; j++){
+            if(gradients[i][j] == 0){
+                continue;
+            }
+            // printf("i,j: %d, %d\n", i, j);
             pii p = {i,j};
-            int dy = (int) (gradY[i][j] / gradients[i][j]);
-            int dx = (int) (gradX[i][j] / gradients[i][j]);
+            double dy =  (gradY[i][j] / gradients[i][j]);
+            double dx =  (gradX[i][j] / gradients[i][j]);
+            printf("dy, dx: %f, %f, %f, %f\n", dy, dx, gradY[i][j], gradX[i][j] );
             for(int r = 1; r < MAX_RADII; r++){
                 pii p_plus =  {p.first + dy * r, p.second + dx * r};
                 pii p_minus = {p.first - dy * r, p.second - dx * r};
@@ -130,32 +138,75 @@ void radialSymmetry(double **gradX, double** gradY, double **gradients, Image *c
 }
 
 //gaussian convolve
-void gaussConvolve(double ***M, double **postGauss, Image *color){
+void gaussConvolve(double ***M, double **postGauss, int **radii, Image *color){
 
     double gauss[3][3] = {
-        {1, 2, 1},
-        {2, 4, 2},
-        {1, 2, 1}
+        {1.0/16.0, 2.0/16.0, 1.0/16.0},
+        {2.0/16.0, 4.0/16.0, 2.0/16.0},
+        {1.0/16.0, 2.0/16.0, 1.0/16.0}
     };
 
+    // for(int i = 0; i < 3; i++){
+    //         for(int j = 0; j < 3; j++){
+    //             gauss[i][j] =  1/16 * gauss[i][j];
+    //     }
+    // }
+
     for(int i = 0; i < color->h; i++){
-        for(int j = 0; j < color->h; j++){
+        for(int j = 0; j < color->w; j++){
             double t = 0;
+            int best_r = 0;
             for(int r = 0; r < MAX_RADII; r++){
-                t += M[r][i][j];
+                // t = max(M[r][i][j], t);
+                if(M[r][i][j] > t){
+                    best_r = r;
+                    t = M[r][i][j];
+                }
             }
+            radii[i][j] = best_r;
             M[0][i][j] = t;
         }
     }
     for(int i = 1; i < color->h - 1; i++){
-        for(int j = 1; j < color->h - 1; j++){
+        for(int j = 1; j < color->w - 1; j++){
+            // printf("(%d, %d)", i, j);
             double g = 0;
             for(int dy = 0; dy < 3; dy++){
                 for(int dx = 0; dx < 3; dx++){
+                    // printf("(%d, %d), %f || ", dy, dx, gauss[dy][dx]);
                     g += M[0][dy+i-1][dx+j-1] * gauss[dy][dx];
+                    if(g != 0){
+                        // printf("(%d,%d) %f, %f\n", i, j, M[0][dy+i-1][dx+j-1], gauss[dy][dx]);
+                    }
                 }
             }
-            postGauss[i][j] = g;
+            // printf("\n");
+            // printf("(%d,%d)->%f\n", i,j, g);
+            postGauss[i][j] = (g > THRESH) ? g : 0;
+        }
+    }
+}
+
+void postGaussNMS(double **postGauss, double **gaussNMS, int **radii,Image* color){
+    for(int i = 1; i < color->h -1; i++){
+        for(int j = 1; j < color->w -1; j++){
+            double maxNeighbor = 0.0;
+            for(int dy = -1; dy < 2; dy++){
+                for(int dx = -1; dx < 2; dx++){
+                    if(dy != 0 || dx != 0){
+                        if(maxNeighbor < postGauss[i+dy][j+dx]){
+                            maxNeighbor = postGauss[i+dy][j+dx];
+                        }
+                        // maxNeighbor = max(maxNeighbor, postGauss[i + dy][j + dx], maxNeighbor);
+                    }
+                }
+            }
+            if(postGauss[i][j] >= maxNeighbor){
+                gaussNMS[i][j] = postGauss[i][j];
+            }
+            else {
+                radii[i][j] = 0;
+            }
         }
     }
 }
@@ -208,14 +259,14 @@ int main(){
             color->img[i][j].b = c;
         }
     }
-
-    printf("%d, %d, %d, \n %d, %d, %d\n", 
-            color->img[0][0].r, color->img[0][0].g, color->img[0][0].b,
-            color->img[399][0].r, color->img[399][0].g, color->img[399][0].b);
+    printf("file read, (%d x %d), %d\n", color->h, color->w,(h-1));
+    // printf("%d, %d, %d, \n %d, %d, %d\n", 
+    //         color->img[0][0].r, color->img[0][0].g, color->img[0][0].b,0, 0, 0);
+    //         // color->img[h-1][0].r, color->img[h-1][0].g, color->img[h-1][0].b);
 
     unsigned long start = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
-    double **grayscaleImage, **gradients, **gradientDir, **nms_grads, **gradX, **gradY;
+    double **grayscaleImage, **gradients, **gradientDir, **nms_grads, **gradX, **gradY, **gaussNms;
     grayscaleImage = (double**)calloc(color->h, sizeof(double*));
     for(int i = 0; i < color->h; i++) grayscaleImage[i] = (double*) calloc(color->w, sizeof(double));
     gradients = (double**)calloc(color->h, sizeof(double*));
@@ -231,9 +282,10 @@ int main(){
     gradY = (double**)calloc(color->h, sizeof(double*));
     for(int i = 0; i < color->h; i++) gradY[i] = (double*) calloc(color->w, sizeof(double));
 
-
+    printf("arrs init\n");
 
     grayscale(color, grayscaleImage);
+    printf("grayscaled\n");
     ofstream grayfile("images/" + basefile + "_gray.txt");
      for(int i = 0; i < color->h; i++){
         for(int j = 0; j < color->w; j++){
@@ -241,9 +293,9 @@ int main(){
         }
     }
     grayfile.close();
-    printf("%d, %d, %d, \n %f, %f, %f\n", 
-            color->img[0][0].r, color->img[0][0].g, color->img[0][0].b,
-           grayscaleImage[399][0], grayscaleImage[399][0], grayscaleImage[399][0]);
+    // printf("%d, %d, %d, \n %f, %f, %f\n", 
+    //         color->img[0][0].r, color->img[0][0].g, color->img[0][0].b,
+    //        grayscaleImage[h-1][0], grayscaleImage[h-1][0], grayscaleImage[h-1][0]);
 
     gradient(grayscaleImage, gradients, gradientDir, color, gradX, gradY);
 
@@ -255,6 +307,18 @@ int main(){
         }
     }
     gradientfile.close();
+
+    ofstream xgradfile("images/" + basefile + "_xgr.txt");
+    ofstream ygradfile("images/" + basefile + "_ygr.txt");
+    printf("writing grads\n");
+    for(int i = 0; i < color->h; i++){
+        for(int j = 0; j < color->w; j++){
+            xgradfile << gradX[i][j] << endl;
+            ygradfile << gradY[i][j] << endl;
+        }
+    }
+    xgradfile.close();
+    ygradfile.close();
 
     ofstream grad_dir_file("images/" + basefile + "_gradsdir.txt");
     printf("writing grads\n");
@@ -287,14 +351,15 @@ int main(){
         }
     }
 
-    radialSymmetry(gradX, gradY, gradients, color, O, M);
+    radialSymmetry(gradX, gradY, nms_grads, color, O, M);
     ofstream rad_sym_file("images/" + basefile + "_radsym.txt");
     printf("writing grads\n");
     for(int i = 0; i < color->h; i++){
         for(int j = 0; j < color->w; j++){
             double tmp_write = 0.0;
             for(int r = 0; r < MAX_RADII; r++){
-                tmp_write += M[r][i][j];
+                tmp_write = max(M[r][i][j], tmp_write);
+                // if(tmp_write) printf("TMP: %f\n", tmp_write);
             }
             rad_sym_file << tmp_write << endl;
         }
@@ -316,6 +381,21 @@ int main(){
     }
     gauss_file.close();
 
+    gaussNms = (double**)calloc(color->h, sizeof(double*));
+    for(int i = 0; i < color->h; i++) gaussNms[i] = (double*) calloc(color->w, sizeof(double));
+
+    int **best_radii;
+    best_radii = (int**)calloc(color->h, sizeof(int*));
+    for(int i = 0; i < color->h; i++) best_radii[i] = (int*) calloc(color->w, sizeof(int));
+
+    postGaussNMS(postGauss, gaussNms, best_radii, color);
+    ofstream post_gauss_nms("images/" + basefile + "_gaussnms.txt");
+    for(int i = 0; i < color->h; i++){
+        for(int j = 0; j < color->w; j++){
+            post_gauss_nms << gaussNms[i][j] << endl;
+        }
+    }
+    post_gauss_nms.close();
 
     unsigned long end = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
